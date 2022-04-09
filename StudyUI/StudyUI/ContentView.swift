9ab20198c3cb293912c6cd214842ec34e2c471e5
gamza-park <youuy28@gmail.com>
2022-04-09 11:51:32 +0900

    Add Project

---
+++ StudyUI/StudyUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudyUI
+//
+//  Created by 박형일 on 2022/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyUI/StudyUI/StudyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyUIApp.swift
+//  StudyUI
+//
+//  Created by 박형일 on 2022/04/09.
+//
+
+import SwiftUI
+
+@main
+struct StudyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
